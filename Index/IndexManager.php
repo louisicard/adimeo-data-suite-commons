f2c37ad20c8695d5e9540f507ae52d44de5f4ec2
Louis Sicard <lsicard@core-techs.fr>
2018-10-24 12:44:54 +0200

    Init IndexManager

--- Index/IndexManager.php
+++ Index/IndexManager.php
@@ -2,8 +2,11 @@
 
 namespace AdimeoDataSuite\Bundle\CommonsBundle\Index;
 
+use AdimeoDataSuite\Bundle\CommonsBundle\Model\PersistentObject;
+use AdimeoDataSuite\Bundle\SecurityBundle\Security\User;
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
 class IndexManager
 {
@@ -16,25 +19,187 @@ class IndexManager
    */
   private $client;
 
-  public function __construct($elasticsearchServerUrl) {
+  /**
+   * @var PasswordEncoderInterface
+   */
+  private $passwordEncoder;
+
+  public function __construct($elasticsearchServerUrl, $passwordEncoder = null) {
     $clientBuilder = new ClientBuilder();
     if(!defined('JSON_PRESERVE_ZERO_FRACTION')){
       $clientBuilder->allowBadJSONSerialization();
     }
     $clientBuilder->setHosts(array($elasticsearchServerUrl));
     $this->client = $clientBuilder->build();
+    $this->passwordEncoder = $passwordEncoder;
   }
 
-  /**
-   * @return Client
-   */
-  public function getClient() {
-    return $this->client;
+
+  public function getServerInfo() {
+    return array(
+      'server_info' => $this->client->info(),
+      'health' => $this->client->cluster()->health(),
+      'stats' => $this->client->cluster()->stats(),
+    );
   }
 
-  public function getServerInfo()
+  public function getIndicesList() {
+    $mappings = $this->client->indices()->getMapping();
+    $settings = $this->client->indices()->getSettings();
+    $indices = $this->client->indices()->stats()['indices'];
+    foreach($indices as $index => $stats) {
+      if(isset($settings[$index])) {
+        $indices[$index]['settings'] = $settings[$index]['settings'];
+      }
+      if(isset($mappings[$index])) {
+        $indices[$index]['mappings'] = $mappings[$index]['mappings'];
+      }
+    }
+    ksort($indices);
+    return $indices;
+  }
+
+  public function getIndex($indexName) {
+    return $this->client->indices()->getSettings(array('index' => $indexName));
+  }
+
+  public function createIndex($indexName, $settings) {
+    $params = array(
+      'index' => $indexName,
+      'body' => array(
+        'settings' => $settings,
+      )
+    );
+    return $this->client->indices()->create($params);
+  }
+
+  public function deleteIndex($indexName) {
+    $params = array(
+      'index' => $indexName
+    );
+    return $this->client->indices()->delete($params);
+  }
+
+  public function createMapping($indexName, $mappingName, $mapping) {
+    $this->client->indices()->putMapping(array(
+      'index' => $indexName,
+      'type' => $mappingName,
+      'body' => $mapping
+    ));
+  }
+
+  function getMapping($indexName, $mappingName)
   {
-    return $this->getClient()->info();
+    try {
+      $mapping = $this->client->indices()->getMapping(array(
+        'index' => $indexName,
+        'type' => $mappingName,
+      ));
+      if (isset($mapping[$indexName]['mappings'][$mappingName])) {
+        return $mapping[$indexName]['mappings'][$mappingName];
+      } else
+        return null;
+    } catch (\Exception $ex) {
+      return null;
+    }
+  }
+
+  public function initStore() {
+    $indices = array_keys($this->getIndicesList());
+    if(!in_array(static::APP_INDEX_NAME, $indices)) {
+      $json = json_decode(file_get_contents(__DIR__ . '/../Resources/store_structure.json'), TRUE);
+      $this->createIndex(static::APP_INDEX_NAME, $json['index']);
+    }
+    $mapping = $this->getMapping(static::APP_INDEX_NAME, 'store_item');
+    if($mapping == null) {
+      $json = json_decode(file_get_contents(__DIR__ . '/../Resources/store_structure.json'), TRUE);
+      $this->createMapping(static::APP_INDEX_NAME, 'store_item', array('properties' => $json['mapping']));
+    }
+    $users = $this->listObjects('user');
+    if(empty($users)) {
+      $user = new User('admin', array('ROLE_ADMIN'), 'admin@example.com', 'Administrator', array());
+      $encoded = $this->passwordEncoder->encodePassword($user, 'admin');
+      $user->setPassword($encoded);
+      $this->persistObject($user);
+    }
+  }
+
+  public function search($indexName, $query, $type = '') {
+    return $this->client->search(array(
+      'index' => $indexName,
+      'type' => $type,
+      'body' => $query
+    ));
+  }
+
+  public function persistObject(PersistentObject $o) {
+    $params = array(
+      'index' => static::APP_INDEX_NAME,
+      'type' => 'store_item',
+      'body' => array(
+        'name' => $o->getName(),
+        'type' => $o->getType(),
+        'data' => $o->serialize()
+      )
+    );
+    if($o->getId() != null) {
+      $params['id'] = $o->getId();
+    }
+    $r = $this->client->index($params);
+    if(isset($r['_id'])){
+      $o->setId($r['_id']);
+    }
+    $this->client->indices()->flush();
+    return $o;
+  }
+
+  public function findObject($type, $id) {
+    $query = array(
+      'query' => array(
+        'bool' => array(
+          'must' => array(
+            array(
+              'term' => array(
+                'type' => $type
+              )
+            ),
+            array(
+              'ids' => array(
+                'values' => array($id)
+              )
+            )
+          )
+        )
+      )
+    );
+    $r = $this->search(static::APP_INDEX_NAME, $query);
+    if(isset($r['hits']['hits'][0])) {
+      return unserialize($r['hits']['hits'][0]['_source']['data']);
+    }
+    else {
+      return null;
+    }
+  }
+
+  public function listObjects($type, $from = 0, $size = 20, $order = 'asc') {
+    $query = array(
+      'query' => array(
+        'term' => array(
+          'type' => $type
+        )
+      ),
+      'size' => $size,
+      'from' => $from,
+      'sort' => array(
+        'name.raw' => $order
+      )
+    );
+    $r = $this->search(static::APP_INDEX_NAME, $query);
+    $objects = [];
+    foreach($r['hits']['hits'] as $hit) {
+      $objects[] = unserialize($hit['_source']['data']);
+    }
+    return $objects;
   }
 
 }
---
+++ Model/PersistentObject.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AdimeoDataSuite\Bundle\CommonsBundle\Model;
+
+abstract class PersistentObject
+{
+
+  abstract function getId();
+  abstract function setId($id);
+  abstract function getName();
+  abstract function getType();
+  public function serialize() {
+    return serialize($this);
+  }
+
+  public static function unserialize($str) {
+    return unserialize($str);
+  }
+
+}
