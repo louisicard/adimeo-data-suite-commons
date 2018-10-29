d0d26091a6f8e6178d7dbfc9d63cc7ca3880fc75
Louis Sicard <lsicard@core-techs.fr>
2018-10-29 11:42:04 +0100

    matching list

---
+++ Model/MatchingList.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AdimeoDataSuite\Model;
+
+
+class MatchingList extends PersistentObject
+{
+  private $id;
+  private $name;
+  private $list;
+
+  function __construct($name, $list = '{}', $id = null) {
+    $this->name = $name;
+    $this->list = $list;
+    $this->id = $id;
+  }
+
+  function getId()
+  {
+    return $this->id;
+  }
+
+  function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  function getName()
+  {
+    return $this->name;
+  }
+
+  function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  function getType()
+  {
+    return 'matching_list';
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getList()
+  {
+    return $this->list;
+  }
+
+  /**
+   * @param mixed $list
+   */
+  public function setList($list)
+  {
+    $this->list = $list;
+  }
+
+
+
+}
