4f8856d10ed55d53ccda548620cde76358867d52
Louis Sicard <lsicard@core-techs.fr>
2018-10-30 11:49:36 +0100

    Boost query

---
+++ Model/BoostQuery.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace AdimeoDataSuite\Model;
+
+
+class BoostQuery extends PersistentObject
+{
+  /** @var  string */
+  private $id;
+  /** @var  string */
+  private $target;
+  /** @var  string */
+  private $definition;
+
+  /**
+   * BoostQuery constructor.
+   * @param string $id
+   * @param string $target
+   * @param string $definition
+   */
+  public function __construct($id, $target, $definition)
+  {
+    $this->id = $id;
+    $this->target = $target;
+    $this->definition = $definition;
+  }
+
+  /**
+   * @return string
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * @param string $id
+   */
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  /**
+   * @return string
+   */
+  public function getTarget()
+  {
+    return $this->target;
+  }
+
+  /**
+   * @param string $target
+   */
+  public function setTarget($target)
+  {
+    $this->target = $target;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDefinition()
+  {
+    return $this->definition;
+  }
+
+  /**
+   * @param string $definition
+   */
+  public function setDefinition($definition)
+  {
+    $this->definition = $definition;
+  }
+
+  function getName()
+  {
+    return 'Boosting query for ' . $this->getTarget();
+  }
+
+  function getType()
+  {
+    return 'boost_query';
+  }
+
+
+}
