c02fe423653b4597b36260549af9f7dd7607e4c6
Louis Sicard <lsicard@core-techs.fr>
2018-11-08 10:00:08 +0100

    Dummy output manager

---
+++ Model/DummyOutputManager.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AdimeoDataSuite\Model;
+
+
+class DummyOutputManager implements OutputManager
+{
+  function writeLn($text)
+  {
+    //nothing to say
+  }
+
+  function dumpArray($array)
+  {
+    //nothing to say
+  }
+
+}
